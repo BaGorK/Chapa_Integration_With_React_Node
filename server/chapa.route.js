fba29bc6fa46e0e46a59160a422ef5e7b29abff4
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-10-29 12:10:30 +0300

    feat: update chapa controller

--- server/chapa.route.js
+++ server/chapa.route.js
@@ -1,25 +1,35 @@
 const express = require('express');
+const axios = require('axios');
 
 const chapaRouter = express.Router();
 
 chapaRouter.post('/accept-payment', async (req, res) => {
-  console.log(req.body);
+  const body = {
+    amount: 120,
+    currency: 'ETB',
+    email: 'abebech_bekele@gmail.com',
+    first_name: 'Edmealem',
+    last_name: 'Kassahun',
+    phone_number: '0912345678',
+    tx_ref: 'chewatatest-6669',
+    return_url: 'http://localhost:5173/',
+    callback_url: 'https://webhook.site/077164d6-29cb-40df-ba29-8a00e59a7e60',
+  };
   try {
     const response = await axios.post(
       'https://api.chapa.co/v1/transaction/initialize',
-      req.body,
+      JSON.stringify(body),
       {
         headers: {
-          Authorization: `Bearer ${CHAPA_SECRET_KEY}`,
+          Authorization: `Bearer ${process.env.CHAPA_SECRET_KEY}`,
           'Content-Type': 'application/json',
         },
       }
-    );
-
-    console.log(response.data);
+    );h
 
     res.status(200).json(response.data);
   } catch (e) {
+    console.log(e);
     res.status(400).json({
       error_code: e.code,
       message: e.message,
